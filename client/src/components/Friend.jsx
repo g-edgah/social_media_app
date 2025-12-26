4693b5b906c3f52728333ff01877a09eb6a43424
g-edgah <172597903+g-edgah@users.noreply.github.com>
2025-12-26 16:54:19 +0400

    friend

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -1,24 +1,33 @@
 import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import { setFriends } from "../state/state.js";
-import FlexBetween from "./styled/flexBetween.jsx";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
+import { setFriends } from "../state/state.js";
+import FlexBetween from "./styled/flexBetween.jsx";
+import UserImage from "./UserImage.jsx";
+
 
 const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { palette } = useTheme();
   const token = useSelector((state) => state.token);
-  const loggedInUserId = useSelector((state) => state.user._id);
+  const { _id } = useSelector((state) => state.user);
   const friends = useSelector((state) => state.user.friends);
-  const isFriend = friends.some((friend) => friend._id === friendId);
+
+  const primaryLight = palette.primary.light;
+  const primaryDark = palette.primary.dark;
+  const medium = palette.neutral.medium;
+  const main = palette.neutral.main;
 
   const api_url = import.meta.env.VITE_API_URL;
 
+  const isFriend = friends.find((friend) => friend._id === friendId);
+
   const patchFriend = async () => {         
-    const response = await fetch(`${api_url}/users/${loggedInUserId}/${friendId}`, {
+    const response = await fetch(`${api_url}/users/${_id}/${friendId}`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -32,22 +41,16 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   return (
     <FlexBetween>
       <FlexBetween gap="1rem">
+        <UserImage image={userPicturePath} size="55px" />
         <Box
           onClick={() => {
             navigate(`/profile/${friendId}`);
             navigate(0);
           }}
         >
-          <img
-            src={`${api_url}/assets/${userPicturePath}`}
-            alt="friend"
-            style={{ width: "55px", height: "55px", borderRadius: "50%", objectFit: "cover" }}
-          />
-        </Box>
-        <Box>
           <Typography
             variant="h5"
-            color={palette.neutral.dark}
+            color={main}
             fontWeight="500"
             sx={{
               "&:hover": {
@@ -58,7 +61,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
           >
             {name}
           </Typography>
-          <Typography color={palette.neutral.medium}>{subtitle}</Typography>
+          <Typography color={medium} fontSize='0.75rem'>{subtitle}</Typography>
         </Box>
       </FlexBetween>
       <IconButton
@@ -66,9 +69,9 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
         sx={{ backgroundColor: palette.primary.light, p: "0.6rem" }}
       >
         {isFriend ? (
-          <PersonRemoveOutlined sx={{ color: palette.primary.dark }} />
+          <PersonRemoveOutlined sx={{ color: primaryDark }} />
         ) : (
-          <PersonAddOutlined sx={{ color: palette.primary.dark }} />
+          <PersonAddOutlined sx={{ color: primaryDark }} />
         )}
       </IconButton>
     </FlexBetween>
